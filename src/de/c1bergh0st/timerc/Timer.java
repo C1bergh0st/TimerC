e63c27426667a27cda135e0c7cdcdfdaffe41cc5
c1bergh0st <philipp.berdesinski@web.de>
2019-07-10 14:18:39 +0200

    Timer and partial TimerManager implementation

--- src/de/c1bergh0st/timerc/Timer.java
+++ src/de/c1bergh0st/timerc/Timer.java
@@ -8,5 +8,59 @@ public class Timer {
     private long duration;
     private long start;
     private long end;
+    private boolean valid;
+
+    public Timer(String name, String message, long duration, boolean looping, boolean soundsOnEnd) {
+        this.name = name;
+        this.message = message;
+        this.soundsOnEnd = soundsOnEnd;
+        this.looping = looping;
+        this.duration = duration;
+        this.valid = true;
+        reset();
+    }
+
+    public Timer(String name, String message, long duration){
+        this(name, message, duration, false, true);
+    }
+
+    public Timer(String name, long duration){
+        this(name, "", duration);
+    }
+
+    public boolean isLooping(){
+        return looping;
+    }
+
+    public boolean shouldSound(){
+        return soundsOnEnd;
+    }
+
+    public boolean hasEnded(){
+        return System.currentTimeMillis() > end;
+    }
+
+    public long getRemaining(){
+        if(this.hasEnded()) return  0;
+        return end - System.currentTimeMillis();
+    }
+
+    public long getEnd() {
+        return end;
+    }
+
+    public void reset(){
+        this.start = System.currentTimeMillis();
+        this.end = start + duration;
+        this.valid = true;
+    }
+
+    public void invalidate(){
+        valid = false;
+    }
+
+    public boolean isValid(){
+        return valid;
+    }
 
 }
--- src/de/c1bergh0st/timerc/TimerController.java
+++ src/de/c1bergh0st/timerc/TimerController.java
@@ -1,9 +1,47 @@
 package de.c1bergh0st.timerc;
 
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class TimerController {
     private List<Timer> timers;
+
+    public TimerController(){
+        timers = new ArrayList<Timer>();
+    }
+
+
+    public void update(){
+        for (Timer timer : timers) {
+            if(timer.hasEnded()){
+                if(timer.shouldSound()){
+                    Toolkit.getDefaultToolkit().beep();
+                }
+                if(timer.isLooping()){
+                    timer.reset();
+                } else {
+                    //Might cause concurrentException, need to check if iterator supports removal
+                    timers.remove(timer);
+                    timer.invalidate();
+                }
+            }
+        }
+    }
+
+    public void add(Timer timer){
+        if(timers.contains(timer)){
+            return;
+        }
+        timers.add(timer);
+        timers.sort(new Comparator<Timer>() {
+            @Override
+            public int compare(Timer timer, Timer t1) {
+                return (int)(timer.getEnd() - t1.getEnd());
+            }
+        });
+    }
 
 
 }
